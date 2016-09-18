bf8f857388a157937dc83229cbb3a92c58205429
Dinne <Dinne@eshangu.com>
2016-09-17 21:13:45 -0500

    modified

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 (function () {
   'use strict';
 
-  angular.module('NameCalculator', [])
-  .controller('NameCalculatorController', function ($scope){
-    $scope.name="";
-    $scope.totalValue=0;
-    $scope.getNameCharCount= function () {
-      var totalCharValue=getNamevaluest($scope.name);
-      $scope.totalValue=totalCharValue;
-    };
-  });
+  angular.module('DIApp', [])
+  .controller('DIController', function ($scope){
+    $scope.name = "name";
+  };
 
-  function getNamevaluest(star){
-    var totalchar=0;
-    for(int i=0; i<star.length; i++){
-      totalchar+=totalchar+star.charCodeAt(i);
-    }
-    alert("totalchar value is "+totalchar);
-    return totalchar;
-
-
-  }
 
 })();
